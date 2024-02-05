c46fc359d7eef30866e9c1d5048ba124e361d0b6
李锦涛 <lijt@eetrust.com>
2024-02-05 17:13:14 +0800

    commit(master):依赖库统一管理

---
+++ buildSrc/src/main/kotlin/DependencyLibrary.kt
@@ -0,0 +1,24 @@
+/**
+ * Desc:
+ * @author lijt
+ * Created on 2024/2/5
+ * Email: lijt@eetrust.com
+ */
+object DependencyLibrary {
+    const val androidxCoreKtx = "androidx.core:core-ktx:1.12.0"
+    const val androidxLifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.7.0"
+    const val androidxActivityCompose = "androidx.activity:activity-compose:1.8.2"
+    const val platformAndroidxComposeBom = "androidx.compose:compose-bom:2023.03.00"
+    const val androidxComposeUi = "androidx.compose.ui:ui"
+    const val androidxComposeUiGraphics = "androidx.compose.ui:ui-graphics"
+    const val androidxComposeUiPreview = "androidx.compose.ui:ui-tooling-preview"
+    const val androidxComposeMaterial3 = "androidx.compose.material3:material3"
+    const val androidxComposeMaterial = "androidx.compose.material:material:1.6.0"
+    const val androidxNavigationCompose = "androidx.navigation:navigation-compose:2.7.6"
+    const val junit = "junit:junit:4.13.2"
+    const val androidxTestExtJunit = "androidx.test.ext:junit:1.1.5"
+    const val androidxTestEspressoCore = "androidx.test.espresso:espresso-core:3.5.1"
+    const val androidxComposeUiTestJunit = "androidx.compose.ui:ui-test-junit4"
+    const val androidxComposeUiTooling = "androidx.compose.ui:ui-tooling"
+    const val androidxComposeUiTestManifest = "androidx.compose.ui:ui-test-manifest"
+}
